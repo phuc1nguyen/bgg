b491d4333f4a0339d00f63d0a75563b3a803bee4
phuc1nguyen <phuc1nguyen@proton.me>
2025-03-18 16:28:11 +0700

    feat: display wallet balance

--- src/features/wallets/components/account.tsx
+++ src/features/wallets/components/account.tsx
@@ -1,18 +1,47 @@
-import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi';
+import {
+  useAccount,
+  useBalance,
+  useChainId,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+  useSwitchChain,
+} from 'wagmi';
 
 export function Account() {
   const { address } = useAccount();
-  const { disconnect } = useDisconnect();
+  const chainId = useChainId();
+  const { chains, switchChain } = useSwitchChain();
   const { data: ensName } = useEnsName({ address });
   const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
+  const { data: balance } = useBalance({ address, chainId });
+  const { disconnect } = useDisconnect();
 
   return (
     <div>
       {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
-      {address && (
-        <div>
-          <b>Address</b>: {ensName ? `${ensName} (${address})` : address}
-        </div>
+      <div>
+        <b>Switch network</b>:
+        {chains.map((chain) => (
+          <button key={chain.id} onClick={() => switchChain({ chainId: chain.id })}>
+            {chain.name}
+          </button>
+        ))}
+      </div>
+      <p>
+        <b>Address</b>: {ensName ? `${ensName} (${address})` : address}
+      </p>
+      {balance && (
+        <>
+          <p>
+            <b>Balance</b>
+          </p>
+          <ul>
+            <li>Value: {balance.formatted}</li>
+            <li>Symbols: {balance.symbol}</li>
+            <li>Decimals: {balance.decimals}</li>
+          </ul>
+        </>
       )}
       <button onClick={() => disconnect()}>Disconnect</button>
     </div>
